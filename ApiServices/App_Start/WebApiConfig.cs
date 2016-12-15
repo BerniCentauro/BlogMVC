4f779bbb6c24893d4cfdb295459d78281f76b92c
Berni Hidalgo <berni@centaurosolutions.com>
2016-12-15 13:09:28 -0600

    Ajax

--- ApiServices/App_Start/WebApiConfig.cs
+++ ApiServices/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace ApiServices
 {
@@ -10,6 +11,10 @@ namespace ApiServices
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // CORS
+            EnableCorsAttribute corsAttr = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsAttr);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ WebInterface/Scripts/app.js
@@ -0,0 +1,38 @@
+﻿
+
+var API_SERVICE_ENDPOINT = 'http://localhost:23543';
+var INTERVAL_TIME = 5000;
+
+// On Document Ready
+$(document).ready(function () {
+    getAllNews();
+
+    setInterval(function () {
+        getAllNews();
+    }, INTERVAL_TIME);
+});
+
+// Get All News
+var getAllNews = function () {
+    var url = API_SERVICE_ENDPOINT + '/api/news';
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function (response, status) {
+            if (status == 'success') {
+                $('#news-list').empty();
+
+                // $('#id-footer').text(response);
+
+                response.forEach(function (item, index) {
+                    $('#news-tmpl').tmpl(item).appendTo('#news-list');
+                });
+            }
+        },
+        error: function (response, status) {
+
+        }
+    });
+}
